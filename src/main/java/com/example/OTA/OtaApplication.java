e600816299b0c8e9251ca9b8d98768c65a9c9bd8
kienboi <164771048+kienboi@users.noreply.github.com>
2026-09-09 20:55:25 +0700

    first commit

---
+++ src/main/java/com/example/OTA/OtaApplication.java
@@ -0,0 +1,13 @@
+package com.example.OTA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OTA/OtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OTA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
