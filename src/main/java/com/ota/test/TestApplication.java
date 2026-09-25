be787b2c2744931244232bd4655de64944b165b4
David Le <23520152@gm.uit.edu.vn>
2026-09-25 23:54:49 +0700

    refactor: migrate project from maven to gradle and restructure packages

--- src/main/java/com/ota/test/TestApplication.java
+++ src/main/java/com/ota/test/TestApplication.java
@@ -1,13 +1,13 @@
-package com.example.OTA;
+package com.ota.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OtaApplication {
+public class TestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OtaApplication.class, args);
+		SpringApplication.run(TestApplication.class, args);
 	}
 
 }
--- src/test/java/com/ota/test/TestApplicationTests.java
+++ src/test/java/com/ota/test/TestApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.OTA;
+package com.ota.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OtaApplicationTests {
+class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
